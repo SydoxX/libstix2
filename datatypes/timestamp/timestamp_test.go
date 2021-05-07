4098f7d02e1cc284afe0c2c518eba6a7576200b4
Vojtěch Boček <vojtech.bocek@avast.com>
2021-05-07 15:48:35 +0200

    Fix tests

--- datatypes/timestamp/timestamp_test.go
+++ datatypes/timestamp/timestamp_test.go
@@ -3,6 +3,7 @@ package timestamp_test
 import (
 	"encoding/json"
 	"testing"
+	"time"
 
 	"github.com/avast/libstix2/datatypes/timestamp"
 )
@@ -30,5 +31,22 @@ func TestJsonEncode(t *testing.T) {
 
 	if string(encoded) != "{}" {
 		t.Fatal("Unexpected encoded value", string(encoded))
+	}
+}
+
+func TestFormat(t *testing.T) {
+	ts := timeStrDirect{
+		FirstSeen: timestamp.New(time.Date(2021, 01, 01, 12, 0, 0, 0,
+			time.FixedZone("test", 3600))),
+	}
+
+	encoded, err := json.Marshal(&ts)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	const expected = `{"first_seen":"2021-01-01T11:00:00.000000Z"}`
+	if string(encoded) != expected {
+		t.Fatalf("Encoded timestamp has wrong format: %s !=%s", string(encoded), expected)
 	}
 }
