b7bc1ac7baab09acb72a605140e7963825be0514
Vojtěch Boček <vojtech.bocek@avast.com>
2021-05-07 13:18:28 +0200

    Force formatting of timestamps with 3 decimal places
    As per stix2-validator

--- datatypes/timestamp/timestamp.go
+++ datatypes/timestamp/timestamp.go
@@ -6,9 +6,12 @@
 package timestamp
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
+
+const Format = "2006-01-02T15:04:05.000Z07:00"
 
 type Timestamp struct {
 	time.Time
@@ -18,7 +21,19 @@ func (t *Timestamp) MarshalJSON() ([]byte, error) {
 	if t.IsZero() {
 		return nil, fmt.Errorf("Encoding Zero value timestamp is prohibited.")
 	}
-	return t.UTC().MarshalJSON()
+
+	// From time.MarshalJSON
+	if y := t.Year(); y < 0 || y >= 10000 {
+		// RFC 3339 is clear that years are 4 digits exactly.
+		// See golang.org/issue/4556#c15 for more discussion.
+		return nil, errors.New("Time.MarshalJSON: year outside of range [0,9999]")
+	}
+
+	b := make([]byte, 0, len(Format)+2)
+	b = append(b, '"')
+	b = t.UTC().AppendFormat(b, Format)
+	b = append(b, '"')
+	return b, nil
 }
 
 func New(t time.Time) Timestamp {
