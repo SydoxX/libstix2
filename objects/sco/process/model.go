cc1ec5386f3ca3629db92ead6d7cb04a064a37bb
Konstantin Papesh <konstantin.papesh@fh-hagenberg.at>
2022-09-06 13:38:49 +0200

    Altered objects to be compatible with avast changes

--- objects/sco/process/model.go
+++ objects/sco/process/model.go
@@ -6,7 +6,10 @@
 package process
 
 import (
-	"github.com/freetaxii/libstix2/objects"
+	"github.com/avast/libstix2/objects"
+	"github.com/avast/libstix2/objects/common"
+	"github.com/avast/libstix2/objects/factory"
+	"github.com/avast/libstix2/objects/properties"
 )
 
 // ----------------------------------------------------------------------
@@ -20,8 +23,8 @@ All of the methods not defined local to this type are inherited from the
 individual properties.
 */
 type Process struct {
-	objects.CommonObjectProperties
-	objects.ExtensionsProperty
+	common.CommonObjectProperties
+	properties.ExtensionsProperty
 	IsHidden             bool              `json:"is_hidden,omitempty" bson:"is_hidden,omitempty"`
 	Pid                  int               `json:"pid,omitempty" bson:"pid,omitempty"`
 	CreatedTime          string            `json:"created_time,omitempty" bson:"created_time,omitempty"`
@@ -35,39 +38,20 @@ type Process struct {
 	ChildRefs            []string          `json:"child_refs,omitempty" bson:"child_refs,omitempty"`
 }
 
-/*
-GetPropertyList - This method will return a list of all of the properties that
-are unique to this object. This is used by the custom UnmarshalJSON for this
-object. It is defined here in this file to make it easy to keep in sync.
-*/
-func (o *Process) GetPropertyList() []string {
-	return []string{
-		"extensions",
-		"is_hidden",
-		"pid",
-		"created_time",
-		"cwd",
-		"command_line",
-		"environment_variables",
-		"opened_connection_refs",
-		"creator_user_ref",
-		"image_ref",
-		"parent_ref",
-		"child_refs",
-	}
+func init() {
+	factory.RegisterObjectCreator(objects.TypeProcess, func() common.STIXObject {
+		return New()
+	})
 }
 
-// ----------------------------------------------------------------------
-// Initialization Functions
-// ----------------------------------------------------------------------
-
-/*
-New - This function will create a new STIX Process SCO and return it as a
-pointer. It will also initialize the object by setting all of the basic
-properties.
-*/
 func New() *Process {
 	var obj Process
-	obj.InitSCO("process")
+	obj.InitSCO(objects.TypeProcess)
 	return &obj
+}
+
+func (o *Process) Valid() []error {
+	errors := o.CommonObjectProperties.ValidSDO()
+
+	return errors
 }
